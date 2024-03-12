6ffd8d0b75a7341daf47b58fee562b4d73a9308a
rsesham18 <149911474+rsesham18@users.noreply.github.com>
2024-03-11 22:18:32 -0700

    Add files via upload

--- top-3.sv
+++ top-3.sv
@@ -28,18 +28,20 @@ wire [7:0]  Data;
 
 
 Intel8088 P(CLK, MNMX, TEST, RESET, READY, NMI, INTR, HOLD, AD, A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN);
-MEMORY_1 #(0) M1(.CS(CS_0),.Data(Data),.*);
+/*MEMORY_1 #(0) M1(.CS(CS_0),.Data(Data),.*);
 MEMORY_2 #(0) M2(.CS(CS_1),.Data(Data),.RD(RD),.WR(WR),.Address(Address),.*);
 IO_1 #(1) I1(.CS(CS_2),.*);
-IO_2 #(1) I2(.CS(CS_3),.*);
+IO_2 #(1) I2(.CS(CS_3),.*);*/
+
+MEMORY_IO #(0) M1(.CS(CS_0),.Data(Data),.*);
+MEMORY_IO #(0) M2(.CS(CS_1),.Data(Data),.RD(RD),.WR(WR),.Address(Address),.*);
+MEMORY_IO #(1) I1(.CS(CS_2),.*);
+MEMORY_IO #(1) I2(.CS(CS_3),.*);
 
 assign CS_0 = Address[19] == 1;
 assign CS_1 = Address[19] ==0;
-assign CS_2= (Address[15:4] == 12'hFF0) ;
-assign CS_3= (Address[15:9] == 7'h0E) ;
-
-//assign CS_2 = (Address >= 16'hFF00 && Address <= 16'hFF0F);
-//assign CS_3 = (Address >= 16'h1C00 && Address <= 16'h1D00);
+assign CS_2= Address[15:4] == 12'hFF0 ;
+assign CS_3= Address[15:9] == 7'h0E ;
 
 // 8282 Latch to latch bus address
 always_latch
